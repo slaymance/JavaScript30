9b6e87c9a8a2a8e1fa4de5b4e233c6a059c7da7b
Shane Laymance <shane.laymance@gmail.com>
2017-12-29 12:38:58 -0800

    Implement day 8 canvas exercise

---
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -0,0 +1,48 @@
+/* eslint-env browser */
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 1;
+
+let isDrawing = false;
+let direction = true;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+  if (!isDrawing) return;
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue = hue === 360 ? 0 : hue + 1;
+
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+
+  ctx.lineWidth = direction ?
+    ctx.lineWidth + 1 :
+    ctx.lineWidth - 1;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => { isDrawing = false; });
+canvas.addEventListener('mouseout', () => { isDrawing = false; });
